2a0e3d75e4c89bb4b29eb1576f5665327347d10c
Violet <crystalbomb64@hotmail.com>
2022-02-23 22:31:33 +0400

    Added basis of player class

--- Player.py
+++ Player.py
@@ -1,1 +1,8 @@
-
+class Player:
+    def __init__(self, saved: bool):
+        if saved is True:
+            # Runs if there is a save file to load from
+            pass
+        else:
+            # Runs if this is a new game
+            pass
