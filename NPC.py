cbe8a8739514f8f6f71d8607c00032357134ea7a
Violet <crystalbomb64@hotmail.com>
2022-02-23 22:30:54 +0400

    Added basis of person class

--- NPC.py
+++ NPC.py
@@ -1,1 +1,7 @@
-
+class Person:
+    def __init__(self, name, pronouns, preferences, personality):
+        self.name = name
+        self.pronouns = pronouns
+        self.likes = preferences[0]
+        self.dislikes = preferences[1]
+        self.personality = personality
